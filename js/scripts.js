a170c7345d3905c06c655daba402a12b0bdd159b
brad pizzimenti and jared beckler <jared.beckler@gmail.com>
2016-01-25 16:15:26 -0800

    WIP todo list toggle currenly only works on every other item

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,28 @@ function Todo(itemName) {
   this.itemName = itemName;
 }
 
-$(document).ready( function() {
+$(document).ready(function() {
   $('form#new-item').submit(function(event) {
     event.preventDefault();
 
-    var newItemName = $('input#new-item-name').val();
 
+    var newItemName = $('input#new-item-name').val();
+    var newTodo = new Todo(newItemName);
 
     $('ul#items').append('<li><span class="todo-item">' +
-    '<button class="btn btn-success" >Done</button> ' +
+    '<button class="btn btn-success done-button">Done</button> ' +
     newItemName +'</span></li>');
+
+
+//currently not working
+    // $("span").click(function(){
+    //   if ($("span").hasClass('done-item')) {
+    //     $(this).removeClass('done-item');
+    //   } else {
+    //     $(this).addClass("done-item");
+    //   }
+    // });
+
   });
+
 });
